6b6667136a6314d0c85ed11e09dd60cb819880b9
wen <135192116+wendycaicedo@users.noreply.github.com>
2023-11-16 18:11:13 -0500

    app.py

--- app.py
+++ app.py
@@ -1,39 +1,52 @@
 from flask import Flask,render_template
-import requests
-from dotenv import load_dotenv, dotenv_values
+import requests 
+from dotenv import load_dotenv,dotenv_values 
 
-config=dotenv_values('.env')
+
+config = dotenv_values('.env')
+
 
 app = Flask(__name__)
 
 def get_weather_data(city):
-    API_KEY=config['API_KEY']
-    url=f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=es&units=metric'
-    r= requests.get(url).json()
+    API_KEY = config['API_KEY']
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&lang=es&units=metric'
+    r = requests.get(url).json()
     print(r)
     return r
+
 @app.route('/Prueba')
 def Prueba():
-    Clima= get_weather_data('guayaquil')
+    Clima= get_weather_data('CUENCA')
     temperatura=str (Clima['main']['temp'])
-    descripcion= str (Clima['weather'][0]['description'])
+    descripcion=str (Clima['weather'][0]['description'])
     icono=str (Clima['weather'][0]['icon'])
+
     r_json={
-        'cuidad':'guayaquil',
+        'ciudad': 'CUENCA',
         'temperatura':temperatura,
-        'descripcion': descripcion,
-        'icono':icono,
-        }
-    return render_template('weather.html',Clima=r_json)
+        'descripcion':descripcion,
+        'icono':icono
+    }
+    return render_template ('weather.html', Clima= r_json)
+
+@app.route('/wen')
+def wen():
+    return get_weather_data('CUENCA')
+
+
+@app.route('/weather')
+def weather():
+    return render_template('weather.html')
 
 
 @app.route('/about')
 def about():
-    return render_template ("weather.html")
+    return render_template('about.html')
 
-@app.route('/clima')
-def clima():
-    return 'Clima'
+@app.route('/Clima')
+def Clima ():
+    return 'saber del clima es importante'
 
 if __name__ == '__main__':
     app.run(debug=True)
